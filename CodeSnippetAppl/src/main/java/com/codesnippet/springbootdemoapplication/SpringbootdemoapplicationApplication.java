0e4eef52454ceb1c6e5d70a351a886ff2d9b0e26
Venkata Sai <106716142+Venkatasai-483@users.noreply.github.com>
2024-12-22 10:12:17 +0530

    Add files via upload

---
+++ CodeSnippetAppl/src/main/java/com/codesnippet/springbootdemoapplication/SpringbootdemoapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.codesnippet.springbootdemoapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdemoapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdemoapplicationApplication.class, args);
+	}
+
+}
---
+++ CodeSnippetAppl/src/test/java/com/codesnippet/springbootdemoapplication/SpringbootdemoapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesnippet.springbootdemoapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootdemoapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
